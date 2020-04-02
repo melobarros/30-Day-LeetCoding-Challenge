d6e5c0386e3c978d2088da2cfc6151f142ec03d5
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-02 11:03:03 -0300

    Added solution and problem description

---
+++ Week-1/Day2-HappyNumber.py
@@ -0,0 +1,28 @@
+"""
+Day 2 - Happy Number
+LeetCode Easy Problem
+
+Write an algorithm to determine if a number is "happy".
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+  Input: 19
+  Output: true
+  Explanation: 
+  12 + 92 = 82
+  82 + 22 = 68
+  62 + 82 = 100
+  12 + 02 + 02 = 1
+"""
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = {}
+        
+        while not(n in seen):
+            if sum([int(d)**2 for d in str(n)]) == 1:
+                return True
+            else:
+                seen[n] = True;
+                n = sum([int(d)**2 for d in str(n)])
+        return False
