b7141e35cc21538ed40508c142ec10e2db8a0e49
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-06 17:18:17 -0300

    Added solution

--- Week-1/Day6-GroupAnagrams.py
+++ Week-1/Day6-GroupAnagrams.py
@@ -17,3 +17,20 @@ Note:
   All inputs will be in lowercase.
   The order of your output does not matter.
 """
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        base_anagrams = {}
+        output = []
+        
+        for index, string in enumerate(strs):
+            sortedString = ''.join(sorted(list(string)))
+            if sortedString in base_anagrams:
+                base_anagrams[sortedString].append(string)
+            else:
+                base_anagrams[sortedString] = [string]
+                
+        for anagram_list in base_anagrams.values():
+            output.append(anagram_list)
+            
+        return output
