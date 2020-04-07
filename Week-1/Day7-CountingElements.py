0369f7e82f9d7f680f958ff29785bf4f8510fa00
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-07 09:02:10 -0300

    Update Day7-CountingElements.py

--- Week-1/Day7-CountingElements.py
+++ Week-1/Day7-CountingElements.py
@@ -26,7 +26,8 @@ Example 4:
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         count = 0
-        for num in arr: if(num + 1 in arr): 
+        for num in arr: 
+          if(num + 1 in arr): 
             count += 1
             
         return count
